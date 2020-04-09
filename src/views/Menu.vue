432981ec3add6c8e3302bef204178f2b0cd84ce3
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2020-04-09 15:29:00 +0900

    テーマ適用

---
+++ src/views/Menu.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-list dense>
+    <!-- Home -->
+    <v-list-item link to="/">
+      <v-list-item-content>
+        <v-list-item-title>Top</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
